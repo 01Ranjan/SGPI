c32bcd723cdd8ccc606b8669e85039e3d9a83413
ketan2035 <ketankumar147856@gmail.com>
2025-08-09 20:22:31 +0530

    signup page done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Signup from './pages/Signup'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="bg-amber-400">hekko</div>
+     <Signup/>
     </>
   )
 }
